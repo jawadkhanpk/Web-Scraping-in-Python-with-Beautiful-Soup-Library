f65cce26cc6c37ab9b01043a432cebec23ce89bf
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-07-04 01:32:20 +0500

    Parsing HTML file and making Soup🍲

--- Parsing-HTML-and-Making-Soup.py
+++ Parsing-HTML-and-Making-Soup.py
@@ -1,1 +1,43 @@
-print("Hello World")
+# Beautiful Soup Library
+from bs4 import BeautifulSoup
+
+# LXML Module
+import lxml
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+# If above line doesn't work, then we have to add lxml parser instead
+# soup = BeautifulSoup(contents, "lxml")
+
+
+print(soup.title)
+# Output: <title>Jawad's Personal Site</title>
+
+print(soup.title.string)
+# Output: Jawad's Personal Site
+
+print(soup)
+# This will print overall HTML text of a website
+
+print(soup.prettify())
+# This will print overall HTML text properly indented
+
+print(soup.a)
+# This will give us the first anchor tag that it finds in our website
+
+print(soup.li)
+# This will give us the first list tag that it finds in our website
+
+print(soup.p)
+# This will give us the first paragraph tag that it finds in our website
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+# This will give us all the anchor tags that it finds in our website
+
+all_paragraph_tags = soup.find_all(name="p")
+print(all_paragraph_tags)
+# This will give us all the paragraph tags that it finds in our website
